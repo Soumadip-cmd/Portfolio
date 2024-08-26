0479b8a83054fc97c00e341a05203f6509d66db6
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-08-26 14:41:51 +0530

    msg confirm

---
+++ client/src/components/Test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <>
+      <canvas class="upper-canvas " width="876" height="550" style="cursor: move;"></canvas>
+    </>
+  )
+}
+
+export default Test
